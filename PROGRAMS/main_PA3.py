2128003f8ff5b96d183e5853735b05b30179bbc5
ychen506 <ychen506@jhu.edu>
2021-11-18 01:07:01 -0500

    update main

--- PROGRAMS/main_PA3.py
+++ PROGRAMS/main_PA3.py
@@ -13,16 +13,26 @@ from registration import registration
 # =======================================Configurations========================================
 # =============================================================================================
 
-name = "A"
+name = "J"
 
-first_reading_file = 'PA3-' + name +'-Debug-SampleReadingsTest.txt'
+
+#first_reading_file = 'PA3-' + name +'-Debug-SampleReadingsTest.txt'
+
+first_reading_file = 'PA3-' + name +'-Unknown-SampleReadingsTest.txt'
+
 output_filename = 'PA3-' + name + '-Output.txt'
 debug_output_filename = 'PA3-' + name + '-Debug-Output.txt' 
+
 first_body_filename = 'Problem3-BodyA.txt'
+
 N_A, a_list, a_tip =pa3.read_body(first_body_filename) 
+
 second_body_filename = 'Problem3-BodyB.txt'
+
 N_B, b_list, b_tip =pa3.read_body(second_body_filename)
+
 A_list, B_list, D_list, N_D, N_samples = pa3.read_sample_readings(first_reading_file, N_A, N_B)
+
 d_list = []
 for i in range(N_samples):
     A_sublist = A_list[i*N_A:(i+1)*N_A]
@@ -52,6 +62,8 @@ print('='*5+'Time Elapsed Using Brutal Force:{:.3f}'.format(end-start)+'='*5)
 e = []
 for row in range(N_samples):
     e.append(abs((d_list[row] - c_list[row]).norm()))
+
+"""
 #read debug output file provided by professor
 d_output, c_output, e_output = pa3.get_debug_output(debug_output_filename)
 # compare our output with debug output
@@ -63,7 +75,7 @@ print(average_error_c.x, average_error_c.y, average_error_c.z)
 print('error in e = ')
 print(average_error_e)
 print('\n')
-
+"""
 
 
 # =============================================================================================
@@ -81,18 +93,20 @@ print('='*5+'Time Elapsed Using KDTree:{:.3f}'.format(end-start)+'='*5)
 e = []
 for row in range(N_samples):
     e.append(abs((d_list[row] - c_list[row]).norm()))
+
+"""
 #read debug output file provided by professor
 d_output, c_output, e_output = pa3.get_debug_output(debug_output_filename)
 # compare our output with debug output
 average_error_d, average_error_c, average_error_e = pa3.compare_output(d_list, c_list, e, d_output, c_output, e_output, N_samples)
 print('error in d = ')
-print(average_error_d.x, average_error_d.y, average_error_d.z)
+print(round(average_error_d.x,3), round(average_error_d.y, 3), round(average_error_d.z, 3))
 print('error in c = ')
-print(average_error_c.x, average_error_c.y, average_error_c.z)
+print(round(average_error_c.x, 3), round(average_error_c.y, 3), round(average_error_c.z, 3))
 print('error in e = ')
-print(average_error_e)
+print(round(average_error_e, 3))
 
-
+"""
     
 
 
